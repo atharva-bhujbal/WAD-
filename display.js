d321c139635f1f7bcba2d4f2e7bf3efeacbb4902
Atharva Bhujbal <bhujbalatharva079@gmail.com>
2026-05-14 09:07:30 +0530

    Delete display.js

--- display.js
+++
@@ -1,9 +0,0 @@
-let users = JSON.parse(localStorage.getItem("users")) || [];
-
-let list = document.getElementById("userList");
-
-users.forEach(function(user) {
-    let li = document.createElement("li");
-    li.textContent = user.name + " - " + user.email;
-    list.appendChild(li);
-});
